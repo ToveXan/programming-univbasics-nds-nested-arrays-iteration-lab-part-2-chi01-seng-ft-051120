7effc076906846f9842754dd26248d33f7faceb3
Zoe Xanos <zxanos@gmail.com>
2020-05-10 06:28:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,28 +10,15 @@ def find_min_in_nested_arrays(src)
   
   while row_index< src.count do
     element_index = 0 
-    while element_index < src.length do
+    while element_index < src[row_index].count do
       sort = src[count].sort
       outer_results << sort[count][inner_count]
       inner_count += 1
     end
     count += 1
   end
-  def find_even_values(src)
-  row_index = 0
-  """
-  while row_index < src.count do
-    element_index = 0
-    while element_index < src[row_index].count do
-      if src[row_index][element_index].even?
-        p src[row_index][element_index]
-      end
-      element_index += 1
-    end
-    row_index += 1
-  end
 
-  """
+
     #while element_index < src.length do 
     #  row.sort
     #  if src[row_index][element_index] > minimum
